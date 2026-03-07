075527d5c7d02c83bad7400431ba8eea0af32747
trgirish <girishlucky193@gmail.com>
2026-03-07 20:06:05 +0530

    loops

---
+++ loops/loop.py
@@ -0,0 +1,107 @@
+# c=5432
+# c=str(c)
+# for u in c:
+#     print(u)
+
+# for i in range(0,len(c)):
+#     print(c[i])
+# x=0
+# while x<len(c):
+#     print(c[x])
+#     x+=1
+
+
+# mobiles=[45,54,8,9,30,35]
+# for i in mobiles:
+#     if i>=45:
+#         print("go")
+#     elif i>=35:
+#         print("av")
+#     else:
+#         print("poor")
+
+# students = [{"name":"girish","rollno":"10","status":"persent"},
+# {"name":"girish0","rollno":"11","status":"persent"},
+# {"name":"girish1","rollno":"12","status":"persent"},
+# {"name":"girish3","rollno":"13","status":"absent"}]
+# for i in students:
+#     if i["status"]=="persent":
+#         print(i["name"])
+# lis=[1,4,9,16,25,36,49,54,64,81,100]
+# i=0
+# while i<len(lis):
+#     print(lis[0])
+#     i+=1
+# for i in lis:
+#     if i==4:
+#         print("found",)
+
+# n=5
+# i=1
+# sum=0
+# while i<=n:
+#     sum+=i
+#     i+=1
+# print(sum) 
+
+# n = 123
+# n = str(n)
+# i=0
+# while i<len(n):
+#     print(n[i])
+#     i+=1
+
+# n=121
+# n1=n
+# rev=0
+# while n>0:
+#     ld=n%10
+#     rev=rev*10+ld
+#     # print(ld)
+#     n=n//10
+# print(rev)
+# if rev==n1:
+#     print("num is palindrome")
+# else:
+#     print("num is not palindrome")
+
+# n=123
+# sum=0
+# while n>0:
+#     ld=n%10
+#     sum+=ld
+#     n=n//10
+# print(sum)
+
+# n=153
+# print(type(n))
+# n1=n
+# s=0
+# while n>0:
+#     ld=n%10
+#     s+=ld*ld*ld
+#     n=n//10
+# print(s)
+# if s==n1:
+#     print(f"{s} is a armstrong")
+# else:
+#     print(f"{s} is not a arm")
+
+# a={"123",76,54,"hfg",}
+# print(a[1])
+
+# a={"name":"girish","roll":10,"status":"absent"}
+# print(a["girish"])
+
+dict1={"giri":"present",
+       "dru":"absent",
+       "name":"present",}
+# for i in dict1:
+#     if dict1[i]=="present":
+#         print(f"{i} is present")
+#     else:
+#         print(f"{i} is absent")
+# dict1["n"]="gtire"
+# print(dict1)
+# dict1["giri"]="absent"
+# print(dict1)
---
+++ loops/loops.py
@@ -0,0 +1,49 @@
+# li = [4,3,2,1,4,54,32,4]
+# for i in li:
+#     print(i+1)
+
+# count = 0
+# for i in range(1,51):
+#     if(i%3==0 or i%5==0):
+#         count+=1
+#         # print(i)
+# print(count)
+# x=0
+# for i in range(1,31):
+#     if i%2==0:
+#         x=i+x
+# print(x)
+
+# x=0
+# for i in range(1,31):
+#     if i%2!=0:
+#         x=i*x
+# print(x)
+
+
+# n = 5
+# for i in range(1,11):
+#     print(f"{n}x{i}={n*i}")
+
+# plan=30
+# start=1
+# while start<=plan:
+#     print(f"{start} watch and ngy movies")
+#     start+=1
+
+
+# s="something"
+# for char in s:
+#     print(char)
+  
+# s="something"
+# for i in range(0,len(s)):
+#     print(s[i])
+
+# str="something"
+# char=0
+# while char<len(str):
+#     print(str[char])
+#     char+=1
+
+
